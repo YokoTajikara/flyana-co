083105864d35af094e1c93ae81a77faa2a1717e0
taji-commit <tajikara@tamsan.sg>
2021-01-28 19:08:42 +0900

    コントローラー修正

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -154,8 +154,10 @@ class EmailController extends Controller
             throw new \Exception("failed to insert salesforce data.");
         }
 
+        $value = $request->toArray(); 
+        $region = $value['residence_region'];
 
-        return view('email.thanks');
+        return view('email.thanks',compact('region'));
 
     }
 
